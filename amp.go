e369421ad4df75056acfa9b1f8784ca7e0238bb1
breedx <bri4n.r33d@gmail.com>
2014-03-03 21:17:35 +0000

    test

--- amp.go
+++ amp.go
@@ -42,7 +42,7 @@ func init() {
 func SumRespond(self *amp.Command) {
     for {        
         ask := <- self.Responder
-        if ask = nil { log.Println("ask is nil") } 
+        if ask == nil { log.Println("ask is nil") } 
         //log.Println(ask)
         //m := *ask.Arguments        
         //a, _ := strconv.Atoi(m["a"])
