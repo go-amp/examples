281b979be0c7768f3ebb3d69ce21858e863c5881
breedx <bri4n.r33d@gmail.com>
2014-03-04 14:56:47 +0000

    update

--- amp.go
+++ amp.go
@@ -1,163 +1,103 @@
 package main
 
+//import "net" 
 import "log"
-import "github.com/go-amp/amp"
-import "runtime"
-import "strconv"
-import "flag"
-import "fmt"
+import "fmt" 
 import "time"
-import "sync"
-import "os"
+import "flag"
+import "runtime"
+import "github.com/go-amp/amp"
 
 var NUM_REQUESTS *int
-var test_start time.Time
-var responses_mutex = &sync.Mutex{}
-var responses_back int = 0
-var requests_count int = 0
-var previous int = 0
-var sent_count int = 0
+
 var isClient *bool
 var isServer *bool
 var isClientHost *string
+var sent_count int = 0
+var requests_count int = 0
+var received_back int = 0
+var test_start time.Time
+const SUM_COMMAND string = "Sum"
 
 func KeepAlive() {
     for { 
         runtime.Gosched()
         time.Sleep(1 * time.Second) 
         if *isClient { 
-            log.Println("sent",sent_count,"responses_back",responses_back) 
-            //if previous != 0 && previous != *NUM_REQUESTS && responses_back == previous { log.Panic("ouch") }
-            previous = responses_back
+            log.Println("sent",sent_count,"received_back",received_back)             
         } else { log.Println("requests",requests_count) }
     }
 }
 
-func init() {
-    procs := runtime.NumCPU()
-    log.Println("setting number procs to",procs)
-    runtime.GOMAXPROCS(procs)
-}
-
-func SumRespond(self *amp.Command) {
-    for ask := range self.Responder {        
-        //ask := <- self.Responder
-        if ask == nil { log.Println("ask is nil") } 
-        //log.Println(ask)
-        //m := *ask.Arguments        
-        //a, _ := strconv.Atoi(m["a"])
-        //b, _ := strconv.Atoi(m["b"])
-        //total := a + b        
-        ////log.Println("SumRespond:",a,"+",b,"=",total)
-        //answer := make(map[string]string)
-        //answer["total"] = "11" //strconv.Itoa(total)
-        ////log.Println("SumRespond sending",ask)
-        //responses_mutex.Lock()
-        //requests_count++
-        //responses_mutex.Unlock()
-        //ask.ReplyChannel <- ask       
-        //runtime.Gosched()
-    }
-}
-
-func BuildSumCommand() *amp.Command {
-    /*
-     * Need a better way to make these
-     * */
-    arguments := [2]string{ "a", "b" }
-    response := [1]string{ "total" }    
-    name := "Sum"
-    responder := make(chan *amp.Ask)
-    sumCommand := &amp.Command{name, responder, arguments[:], response[:]}    
-    go SumRespond(sumCommand)
-    return sumCommand
-}
-
 func server() {
-    log.Println("Hello Server!")    
-    commands := make(map[string]*amp.Command)
-    sum := BuildSumCommand()
-    commands[sum.Name] = sum
-    prot := amp.Init(&commands)    
-    err := prot.ListenTCP(":8000")
-    if err != nil { log.Println(err) } else { KeepAlive() }
+    prot := amp.Init()
+    responder := make(chan *amp.AskBox)
+    prot.RegisterResponder(SUM_COMMAND, responder)
+    go do_sum(responder)
+    prot.ListenTCP(":8000")
+    KeepAlive()
 }
 
-func RemoteSum(a int, b int, c *amp.Client, command *amp.Command) (string, error) {
-    callbox := amp.ResourceCallBox()    
-    m := *callbox.Arguments    
-    m["a"] = strconv.Itoa(a)
-    m["b"] = strconv.Itoa(b)    
-    reply := make(chan *amp.CallBox) 
-    go RemoteTrap(reply)   
-    callbox.Callback = reply
-    callbox.Command = command    
-    //log.Println("CallRemote",callbox.Command.Name,*callbox.Arguments)
-    tag, err := c.CallRemote(callbox)         
-    return tag, err
-}
-
-func RemoteTrap(reply chan *amp.CallBox) {
-    //log.Println("remote trapping",reply)    
-    answer := <-reply
-    
-    //m := *answer.Response
-    //a, _ := strconv.Atoi(m["total"])
-    responses_mutex.Lock()
-    responses_back++    
-    //log.Println("RemoteTrap",*answer.Response,"for",*answer.Arguments,"responses_back",responses_back)
-    if responses_back == *NUM_REQUESTS {
-        now := time.Now()
-        fmt.Printf("time taken -- %f\n", float32(now.Sub(test_start))/1000000000.0)
+func do_sum(in chan *amp.AskBox) {
+    for ask := range in {
+        //log.Println(*ask.Args)
+        ask.Response["i"] = "Buenos Vida"
+        requests_count++
+        ask.Reply()
     }
-    responses_mutex.Unlock()
-    amp.RecycleCallBox(answer)
-    //log.Println("done recycling callbox")
+}
+
+func response_trap(in chan *amp.CallBox) { 
+    for reply := range in {
+        //log.Println(*reply.Response)
+        amp.RecycleCallBox(reply)
+        received_back++
+        if received_back == *NUM_REQUESTS {
+            now := time.Now()
+            fmt.Printf("time taken -- %f\n", float32(now.Sub(test_start))/1000000000.0)
+            close(in)
+        }
+    }
 }
 
 func client() {
-    log.Println("Hello Client!")    
-    commands := make(map[string]*amp.Command)
-    sum := BuildSumCommand()
-    commands[sum.Name] = sum
-    prot := amp.Init(&commands)
+    prot := amp.Init()    
     c, err := prot.ConnectTCP(*isClientHost)
-    if err != nil { log.Println(err) } else {  
-        go send_requests(c, sum)
-        //log.Println("responses_back",responses_back)
-        KeepAlive() 
-    }    
+    if err != nil { return }    
+    go send_requests(c)
+    KeepAlive()    
 }
 
-func send_requests(c *amp.Client, sum *amp.Command) {
-    test_start = time.Now()       
-    log.Println("sending",*NUM_REQUESTS,"requests")
+func send_requests(c *amp.Client) {
+    replies := make(chan *amp.CallBox)
+    go response_trap(replies)
+    test_start = time.Now()   
     for i := 1; i <= *NUM_REQUESTS; i++ {
-        //log.Println("client iteration -",i)
-        _, err := RemoteSum(i, 0, c, sum)
-        if err != nil { log.Println(err) 
-        } else { sent_count++ }
+        //send := []byte{0,1,97,0,6,54,54,50,55,49,54,0,1,98,0,1,48,0,4,95,97,115,107,0,5,97,49,99,98,99,0,8,95,99,111,109,109,97,110,100,0,3,83,117,109,0,0}
+        //log.Println("writing",send)
+        box := amp.ResourceCallBox()
+        box.Args["i"] = "hi there!"
+        box.Callback = replies
+        err := c.CallRemote(SUM_COMMAND, box)
+        //_, err := c.Conn.Write(send)
+        if err != nil { log.Println("err",err); break }
+        sent_count++
         runtime.Gosched()
-        
     }
-    log.Println("done sending")
+    
 }
-
-
 
 func main() {
     isServer = flag.Bool("server", false, "use as a server")
     isClient = flag.Bool("client", false, "use as a client")
     isClientHost = flag.String("host","127.0.0.1:8000","host address")
     NUM_REQUESTS = flag.Int("num",100000,"number of requests to do")
-    //log.Println("isServer",isServer)
+    log.Println("hi")
     flag.Parse()    
     if *isServer {
         server()
-    } else if *isClient {
-        f, err := os.Create("/tmp/logfile")
-        log.Println("err",err,f)
-        //log.SetOutput(f)
+    } else if *isClient {        
         client()
-    } else { flag.Usage() }            
+    } else { flag.Usage() }  
 }
+
